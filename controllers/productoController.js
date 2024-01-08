5155576833d5feabc26d1be8710d390f5190a149
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-01-08 12:20:16 -0300

    eliminar producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -19,3 +19,18 @@ exports.agregarProducto = async (req, res) => {
     res.status(500).json({ error: 'Error al agregar un nuevo producto' });
   }
 };
+
+exports.eliminarProducto = async (req, res) => {
+  try {
+    const idProducto = req.params.id; // Obtener el ID del producto desde los parámetros de la solicitud
+    const productoEliminado = await Producto.findByIdAndDelete(idProducto);
+
+    if (!productoEliminado) {
+      // Si no se encuentra el producto con el ID proporcionado, devolver un error 404
+      return res.status(404).json({ error: 'Producto no encontrado' });
+    }
+    res.json({ mensaje: 'Producto eliminado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar el producto' });
+  }
+};
--- routes/produtoRoutes.js
+++ routes/produtoRoutes.js
@@ -5,5 +5,6 @@ const productoController = require('../controllers/productoController');
 
 router.get('/productos', productoController.obtenerProductos);
 router.post('/productos', productoController.agregarProducto);
+router.delete('/productos/:id', productoController.eliminarProducto); // Nueva ruta para eliminar un producto
 
 module.exports = router;
