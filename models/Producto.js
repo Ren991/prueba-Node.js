41d3c882a701b17405a79b9f4d7ea0f9ef673864
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-01-09 11:44:15 -0300

    Cambio en modelo

--- models/Producto.js
+++ models/Producto.js
@@ -5,6 +5,10 @@ const productoSchema = new mongoose.Schema({
   nombre: String,
   precio: Number,
   descripcion: String,
+  marca:String,
+  stock:Number,
+  categoria: String,
+  imagen: String
 });
 
 const Producto = mongoose.model('Producto', productoSchema);
